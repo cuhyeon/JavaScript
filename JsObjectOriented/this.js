e6d252b93757e1dffe9f85ab5581d9d4eadd05e1
cuhyeon <who37865@naver.com>
2019-04-11 22:01:41 +0900

    Update

--- JsObjectOriented/this.js
+++ JsObjectOriented/this.js
@@ -4,7 +4,7 @@ var kim = {
     second : 20,
     sum : function() {
         return this.first+this.second;
-        //this는 메소드가 들어있는 객체를 가리킴
+        //this는 메소드가 들어있는 객체를 가리킴!
     } 
 }
 // console.log("kim.sum(kim.first,kim.second)",kim.sum(kim.first,kim.second));
